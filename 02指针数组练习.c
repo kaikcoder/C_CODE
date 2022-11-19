a91c3d0de5d23c14a533363e60f28984ef7bc226
kaikcoder <1972039334@qq.com>
2022-11-19 15:01:56 +0800

    提交代码

---
+++ 02指针数组练习.c
@@ -0,0 +1,27 @@
+/* ึธี๋สýื้ */
+#include<stdio.h>
+#include<string.h>
+#define N 4
+void sort(char *ss[],int n){
+    int i,j;
+    char *v;
+    for(i=0; i<n-1; i++){
+        for(j=i+1; j<n; j++){
+            if(strcmp(ss[i],ss[j])<0){
+                v = ss[i];
+                ss[i] = ss[j];
+                ss[j] = v;
+            }
+        }
+    }
+
+}
+int main(){
+    char *arr[] = {"65","128","99","986"};
+    int i;
+    sort(arr,N);
+    for(i=0; i<N; i++){
+        printf("%s",arr[i]);
+    }
+    return 0;
+}
